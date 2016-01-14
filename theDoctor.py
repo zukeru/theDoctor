121121a9c6f2213c7716d0c01671d28e4228b253
Grant Zukel <zukeru@gmail.com>
2016-01-13 20:23:11 -0700

    Update theDoctor.py

--- theDoctor.py
+++ theDoctor.py
@@ -11,7 +11,7 @@ def shell_command_execute(command):
 def update_repo():
     command = "cd %s; git fetch origin; git cherry master origin/master; " % os.getcwd()
     git_status = shell_command_execute(command)
-    
+    git_update = "failure"
     if git_status:
         command = "cd %s; git stash;" % os.getcwd()
         shell_command_execute(command)
@@ -54,8 +54,12 @@ def find_current_jobs():
                     job_dict[filename.split('.')[0]] =  {"filename": filename, "cron_timing":cron_timing[0], "cron_args": None}        
             except Exception as e:
                 print e
-        
-    update_jobs(job_dict)
+    
+    if not jobs_list:
+        print "There are currently no jobs to schedule. I will continue on my mission until a job has arrived."   
+    else:
+        print "it has begun"
+        update_jobs(job_dict)
 
 def build_script_location(job_dict, job):
     
@@ -75,35 +79,42 @@ def build_script_location(job_dict, job):
      
 def update_jobs(job_dict):
     #specify the location of crontab, open the file and re-write with new information.
-    #cron_file_location = "/etc/crontab"
-    #cron_file = open(cron_file_location, "w")
+    cron_file_location = "/etc/crontab"
+    cron_file = open(cron_file_location, "w")
     
-    #cron_file.write("SHELL=/bin/bash \n")
-    #cron_file.write("PATH=/sbin:/bin:/usr/sbin:/usr/bin \n")
-    #cron_file.write("MAILTO=root HOME=/ \n")
+    cron_file.write("SHELL=/bin/bash \n")
+    cron_file.write("PATH=/sbin:/bin:/usr/sbin:/usr/bin \n")
+    cron_file.write("MAILTO=root HOME=/ \n")
     for job in job_dict:
         insert_line = build_script_location(job_dict, job)
         print insert_line
-        #cron_file.write(insert_line)
+        cron_file.write(insert_line)
     
-    #cron_file.close()
+    cron_file.close()
+    
 def main():
     first = True
     check_interval = 30
     
     while True:
+        print "The doctor's tardis is in route."
         if first == True:
             #update repo first if there is a difference and replace cron jobs
+            print "This is my first run so I will update my repo to the latest model."
             update_repo()
+            print "I will now find all current jobs to schedule."
             find_current_jobs()
+            print "I am going to sleep for " + str(check_interval)
             time.sleep(check_interval)
             first = False
         else:
+            print "Look for changes to the jobs in the system, if found I will update to the latest model."
             status = update_repo() 
             if status[1] == 'success':
+                print "Holy Bonkers, the Daliks are attacking.."
                 #check the repo files
                 find_current_jobs()
-            
+            print "I'm tired im going to sleep for " + str(check_interval)
             time.sleep(check_interval)            
     
 if __name__ == '__main__': 
