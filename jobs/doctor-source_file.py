03eebefec4f6237f3cc360ff4865744234085aad
EC2 Default User <ec2-user@ip-172-31-36-131.us-west-2.compute.internal>
2016-01-14 03:27:59 +0000

    removed jobs for now

--- jobs/doctor-source_file.py
+++
@@ -1,7 +0,0 @@
-import os
-import datetime
-os.mkdir("~/job_test_tmp_files")
-dt_file_location = "~/job_test_tmp_files/"+str(datetime.datetime.now())
-dt_file = open(cron_file_location, "w")
-dt_file.write("The Daliks are Attacking.")
-dt_file.close()
